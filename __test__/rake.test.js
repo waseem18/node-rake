8d705b4f15be9ee36e9fdf284fb77afdf8362702
Mike Williamson <mike@korora.ca>
2017-02-17 12:06:23 -0500

    Trim leading/trailing spaces from phrases
    This looked like a minor change but it has a significant impact on the
    results returned:
    
    Before:
    ```javascript
        [ ' popular topic models ',
          ' Latent Dirichlet Allocation',
          'LDA stands ',
          ' initially proposed ',
          ' Blei Ng ',
          ' generative model ',
          ' unobserved groups ',
          ' 2003',
          ' mentioned ',
          ' similar',
          ' sets ',
          ' observations ',
          ' explained ',
          ' Jordan ',
          ' explain ',
          ' parts ',
          ' data ',
          ' Wikipedia ' ]
    ```
    
    After:
    ```javascript
        [ 'Latent Dirichlet Allocation',
          'popular topic models',
          'initially proposed',
          'Blei Ng',
          'generative model',
          'LDA stands',
          'unobserved groups',
          'Wikipedia',
          'mentioned',
          '2003',
          'sets',
          'observations',
          'explained',
          'Jordan',
          'explain',
          'parts',
          'data',
          'similar' ]
    
    ```

--- __test__/rake.test.js
+++ __test__/rake.test.js
@@ -8,12 +8,16 @@ describe('rake', () => {
 
   describe('generate', () => {
 
+    let text = "LDA stands for Latent Dirichlet Allocation. As already mentioned it is one of the more popular topic models which was initially proposed by Blei, Ng and Jordan in 2003. It is a generative model which, according to Wikipedia, allows sets of observations to be explained by unobserved groups that explain why some parts of the data are similar."
+
     it('extracts keywords from text', () => {
-
-      let text = "LDA stands for Latent Dirichlet Allocation. As already mentioned it is one of the more popular topic models which was initially proposed by Blei, Ng and Jordan in 2003. It is a generative model which, according to Wikipedia, allows sets of observations to be explained by unobserved groups that explain why some parts of the data are similar."
-
       let results = rake.generate(text)
       expect(results.length).toEqual(18)
+    })
+
+    it('trims leading and trailing spaces from keywords', () => {
+      let [firstKeyword, ...rest] = rake.generate(text)
+      expect(firstKeyword).toEqual("Latent Dirichlet Allocation")
     })
 
   })
